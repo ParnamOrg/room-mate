a2c87509067f798abf0a85ece3dcb1c9ca2ad1b0
Parth Verma <parthverma011@gmail.com>
2023-11-17 00:49:01 +0530

    Resolve route redirect issue for login page

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "../components/authentication/loginForm";
 import ForgotPasswordForm from "../components/authentication/forgotPassword";
 import Dashboard from "../components/dashboard/index";
 
 export default (
   <Router>
     <Routes>
-      <Route path="/" element={<LoginForm />} />
-      <Route path="/forgot_password" element={<ForgotPasswordForm />} />
       <Route path="/" element={<Dashboard />} />
+      <Route path="/forgot_password" element={<ForgotPasswordForm />} />
     </Routes>
   </Router>
 );
