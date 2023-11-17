ddf18c3e8b245ab4198da1651fe7f833b67d4811
Parth Verma <parthverma011@gmail.com>
2023-11-17 20:37:27 +0530

    Resolve login form height issue

--- app/javascript/components/authentication/loginForm.jsx
+++ app/javascript/components/authentication/loginForm.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { currentDomainUrl, csrfToken} from '../../config/variables';
 import Cookies from 'js-cookie';
 import ErrorAlert from "./errorAlert";
-import { useAlertContext } from '../context/alertContext';
 
 const loginForm = () => {
     const [email, setEmail] = useState('');
@@ -12,7 +11,6 @@ const loginForm = () => {
     const [alertOpacity, setAlertOpacity] = useState('');
     const [alertMessage, setAlertMessage] = useState('');
     const [isErrorMessage, setIsErrorMessage] = useState(0);
-    const { showAlert, hideAlert } = useAlertContext();
 
     const handleLogin = async () => {
         try {
@@ -33,22 +31,18 @@ const loginForm = () => {
             if (response.data.logged_in){
                 Cookies.set('authorization_token', `Bearer ${response.data.user.token}`);
                 window.location.href = '/'
+
                 setAlertOpacity(1);
-                setAlertMessage(error.response?.data.messages);
+                setAlertMessage(response.data.messages);
                 setIsErrorMessage(0);
-                showAlert('Login successful!');
             }
         } catch (error) {
-          console.error('Login failed:', error.response?.data || error.message);
           setAlertOpacity(1);
-          if(error.response?.data.messages == 'Signed In Failed - Unauthorized') {
-            setAlertWidth(400);
-          }
+
           setAlertMessage(error.response?.data.messages);
           setEmail('');
           setPassword('');
           setIsErrorMessage(1);
-          showAlert('Login failed. Please try again.');
         }
 
         setTimeout(() => {
@@ -57,46 +51,46 @@ const loginForm = () => {
       };
 
   return(
-	  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="d-flex align-items-center justify-content-center">
       <ErrorAlert message={alertMessage} class={alertOpacity ? 'show-component' : 'fade-component'} onclick={() => {setAlertOpacity('')}} color={isErrorMessage ? '#DF3F32' : '#07bc0c'} />
-	    <div className="jumbotron jumbotron-fluid bg-transparent" style={{marginTop: -100}}>
-	      <div className="container secondary-color">
-            <form>
-    	        <h1 className="display-4 fw-bold">Sign In</h1>
-    	        <p className="lead small-font" style={{fontWeight: 400}}>
-    	          Enter your account details
-    	        </p>
-                <p className="lead small-font fw-bold" style={{fontWeight: 400}}>
-    	          Email Address
-    	        </p>
-                <input className="input-text fw-bold small-font" placeholder="Email Address" type="email" value={email} onChange={(e) => setEmail(e.target.value)}  />
-                <p className="lead small-font fw-bold mt-3" style={{fontWeight: 400}}>
-    	          Password
-    	        </p>
-                <input className="input-text fw-bold small-font" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <div className="d-flex justify-content-between">
-                    <div className="mt-4 d-flex">
-                        <input className="form-checkbox" type="checkbox" />
-                        <label className="small-font fw-bold">
-                            Remember me
-                        </label>
-                    </div>
-                    <Link
-                        to="/forgot_password"
-                        className="small-font fw-bold recover-password mt-4"
-                    >
-                        Recover Password
-                    </Link>
-                </div>
-    	        <button type="button" onClick={handleLogin}
-    	          className="btn btn-lg d-block mt-4 fw-bold small-font input-text d-flex align-items-center justify-content-center button-style"
-    	        >
-    	          Sign In
-    	        </button>
-            </form>
-	      </div>
-	    </div>
-	  </div>
+      <div className="jumbotron jumbotron-fluid bg-transparent mt-5">
+        <div className="container secondary-color">
+          <form>
+              <h1 className="display-4 fw-bold">Sign In</h1>
+              <p className="lead small-font" style={{fontWeight: 400}}>
+                Enter your account details
+              </p>
+              <p className="lead small-font fw-bold" style={{fontWeight: 400}}>
+                Email Address
+              </p>
+              <input className="input-text fw-bold small-font" placeholder="Email Address" type="email" value={email} onChange={(e) => setEmail(e.target.value)}  />
+              <p className="lead small-font fw-bold mt-3" style={{fontWeight: 400}}>
+                Password
+              </p>
+              <input className="input-text fw-bold small-font" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <div className="d-flex justify-content-between">
+                  <div className="mt-4 d-flex">
+                      <input className="form-checkbox" type="checkbox" />
+                      <label className="small-font fw-bold">
+                          Remember me
+                      </label>
+                  </div>
+                  <Link
+                      to="/forgot_password"
+                      className="small-font fw-bold recover-password mt-4"
+                  >
+                      Recover Password
+                  </Link>
+              </div>
+              <button type="button" onClick={handleLogin}
+                className="btn btn-lg d-block mt-4 fw-bold small-font input-text d-flex align-items-center justify-content-center button-style"
+              >
+                Sign In
+              </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 };
 
